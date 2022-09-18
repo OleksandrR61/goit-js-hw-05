43a033d7327f647d02b30c7176c2f3331bd0d66e
OleksandrR61 <a80953031761@gmail.com>
2022-09-18 13:36:44 +0300

    HW-JS#5v0.1

---
+++ js/js1.js
@@ -0,0 +1,21 @@
+// Выполни рефакторинг методов объекта pizzaPalace, расставив отсутствующие this в местах обращения к свойствам
+    // и методам объекта.
+
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
+
